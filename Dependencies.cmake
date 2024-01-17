0483a07527efa5b7d35ee8c336f758fef7116b0a
Johannes Graeter <j.graeter@dotsene.com>
2024-01-17 20:50:08 +0100

    try to fix it

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -7,6 +7,11 @@ function(anywho_setup_dependencies)
   # For each dependency, see if it's
   # already been provided to us by a parent project
   if(NOT TARGET Catch2::Catch2WithMain)
+  # Only set the cxx_standard if it is not set by someone else
+set(CMAKE_CXX_STANDARD 23)
+
+# TODO (johannes-graeter) remove this once libc++ is shipped with std::expected
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
     cpmaddpackage("gh:catchorg/Catch2@3.3.2")
   endif()
 endfunction()
