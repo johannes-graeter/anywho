900f7fd8139857bebe336fca44f7d4f5ce343389
Johannes Graeter <j.graeter@dotsene.com>
2024-01-20 17:42:47 +0100

    use catch2

--- fuzz_test/fuzz_tester.cpp
+++ fuzz_test/fuzz_tester.cpp
@@ -1,21 +1,24 @@
+// #include <format>
+// #include <iostream>
 #include <iterator>
+#include <string>
 #include <utility>
 
-[[nodiscard]] auto sum_values(const uint8_t *Data, size_t Size)
-{
-  constexpr auto scale = 1000;
+// [[nodiscard]] auto sum_values(const uint8_t *Data, size_t Size)
+// {
+//   constexpr auto scale = 1000;
 
-  int value = 0;
-  for (std::size_t offset = 0; offset < Size; ++offset) {
-    value += static_cast<int>(*std::next(Data, static_cast<long>(offset))) * scale;
-  }
-  return value;
-}
+//   int value = 0;
+//   for (std::size_t offset = 0; offset < Size; ++offset) {
+//     value += static_cast<int>(*std::next(Data, static_cast<long>(offset))) * scale;
+//   }
+//   return value;
+// }
 
-// Fuzzer that attempts to invoke undefined behavior for signed integer overflow
-// cppcheck-suppress unusedFunction symbolName=LLVMFuzzerTestOneInput
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+// // Fuzzer that attempts to invoke undefined behavior for signed integer overflow
+// // cppcheck-suppress unusedFunction symbolName=LLVMFuzzerTestOneInput
+extern "C" int LLVMFuzzerTestOneInput([[maybe_unused]] const uint8_t *Data, [[maybe_unused]] size_t Size)
 {
-  // ::print("Value sum: {}, len{}\n", sum_values(Data, Size), Size);
+  //   auto val = sum_values(Data, Size);
   return 0;
 }
