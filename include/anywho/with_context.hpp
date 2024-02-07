6de5dfbc8fd7c5be6314c117acc72aa5be3a06b5
Johannes Graeter <j.graeter@dotsene.com>
2024-02-07 22:04:54 +0100

    add docs

--- include/anywho/with_context.hpp
+++ include/anywho/with_context.hpp
@@ -5,7 +5,16 @@
 #include <expected>
 
 namespace anywho {
-template<typename V, anywho::concepts::Error E>
+/**
+ * @brief Helper to add context to std::expected holding an error.
+ *
+ * @tparam V Type of the expected value
+ * @tparam E Type of the error
+ * @param exp Object to which context will be added
+ * @param context Context to add
+ * @return std::expected<V, E>
+ */
+template<typename V, concepts::Error E>
 inline std::expected<V, E> with_context(std::expected<V, E> &&exp, Context &&context)
 {
   return exp.transform_error([cont = std::move(context)](auto x) mutable {
