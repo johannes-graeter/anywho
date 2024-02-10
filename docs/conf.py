56f276d6756816636b44a6432b4a792c2e2f4487
Johannes Graeter <j.graeter@dotsene.com>
2024-02-10 17:54:52 +0100

    add sphinx

---
+++ docs/conf.py
@@ -0,0 +1,80 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from sphinx.builders.html import StandaloneHTMLBuilder
+import glob
+#import subprocess
+
+# Doxygen
+#subprocess.call('doxygen Doxyfile.in', shell=True)
+
+project = 'Anywho'
+copyright = '2024, Johannes Graeter'
+author = 'Johannes Graeter'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx_sitemap',
+    'sphinx.ext.inheritance_diagram',
+    'breathe'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+highlight_language = 'c++'
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+
+    'logo_only': False,
+
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+html_static_path = ['_static']
+html_baseurl = 'base'
+
+breathe_projects = {
+	"Anywho": "_build/xml/"
+}
+
+breathe_projects_source = {
+    "Anywho" : ( "../include/", [x for x in glob.glob("../include")])
+}
+breathe_default_project = "Anywho"
+breathe_default_members = ('members', 'undoc-members')
+
+
---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+.. Anywho documentation master file, created by sphinx-quickstart
+You can adapt this file completely to your liking, but it should at least contain the root `toctree` directive.
+Welcome to Anywho's documentation!
+======================================================
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+Indices and tables
+==================
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+Anywho
+======
+.. autodoxygenindex::
+   :project: Anywho
