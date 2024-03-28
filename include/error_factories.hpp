fd5b77a52de47a87d2b87c02bbd88ecfea2f0111
Johannes Graeter <j.graeter@dotsene.com>
2024-03-28 13:45:38 +0100

    small optimizations, deactivate SAN because of gcc bug

--- include/error_factories.hpp
+++ include/error_factories.hpp
@@ -22,7 +22,7 @@ template<typename T, concepts::Error E>
 inline std::expected<T, E> make_error(bool has_no_error, T truth_value, E &&error)
 {
   if (has_no_error) {
-    return truth_value;
+    return std::expected<T, E>{ truth_value };
   } else {
     return std::unexpected(error);
   }
@@ -40,7 +40,7 @@ inline std::expected<T, E> make_error(bool has_no_error, T truth_value, E &&erro
 template<typename T, concepts::Error E>
 inline std::expected<T, E> make_error(std::function<std::tuple<bool, T>(void)> callable, E &&error)
 {
-  const auto &[has_no_error, truth_value] = callable();
+  const auto [has_no_error, truth_value] = callable();
 
   return make_error(has_no_error, truth_value, std::move(error));
 }
@@ -56,7 +56,7 @@ inline std::expected<T, E> make_error(std::function<std::tuple<bool, T>(void)> c
 template<typename T> inline std::expected<T, ErrorFromCode> make_error(std::error_code error_code, T truth_value)
 {
   if (!error_code) {
-    return truth_value;
+    return std::expected<T, ErrorFromCode>{ truth_value };
   } else {
     return std::unexpected(ErrorFromCode(error_code));
   }
@@ -73,7 +73,7 @@ template<typename T> inline std::expected<T, ErrorFromCode> make_error(std::erro
 template<typename T>
 inline std::expected<T, ErrorFromCode> make_error(std::function<std::tuple<std::error_code, T>(void)> callable)
 {
-  const auto &[error_code, truth_value] = callable();
+  const auto [error_code, truth_value] = callable();
 
   return make_error(error_code, truth_value);
 }
