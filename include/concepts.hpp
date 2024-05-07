8a7eb4f7226b034649b5be1e6e543a52416d590f
Johannes Graeter <j.graeter@dotsene.com>
2024-05-07 22:26:25 +0200

    add nested ns

--- include/concepts.hpp
+++ include/concepts.hpp
@@ -4,37 +4,35 @@
 #include <concepts>
 #include <string>
 
-namespace anywho {
-namespace concepts {
-  /**
-   * @brief Basic concept for all errors
-   *
-   * @tparam T
-   */
-  template<typename T>
-  concept Error = requires(T &obj, Context c) {
-    {
-      obj.format()
-    } -> std::convertible_to<std::string>;
-    {
-      obj.message()
-    } -> std::convertible_to<std::string>;
-    {
-      obj.id()
-    } -> std::unsigned_integral;
-    obj.consume_context(std::move(c));
-  };
+namespace anywho::concepts {
+/**
+ * @brief Basic concept for all errors
+ *
+ * @tparam T
+ */
+template<typename T>
+concept Error = requires(T &obj, Context c) {
+  {
+    obj.format()
+  } -> std::convertible_to<std::string>;
+  {
+    obj.message()
+  } -> std::convertible_to<std::string>;
+  {
+    obj.id()
+  } -> std::unsigned_integral;
+  obj.consume_context(std::move(c));
+};
 
-  /**
-   * @brief Concept for everything that can be catched, mainly exception like objects
-   *
-   * @tparam T
-   */
-  template<typename T>
-  concept Catchable = requires(T &obj) {
-    {
-      obj.what()
-    } -> std::convertible_to<std::string>;
-  };
-}// namespace concepts
-}// namespace anywho
+/**
+ * @brief Concept for everything that can be catched, mainly exception like objects
+ *
+ * @tparam T
+ */
+template<typename T>
+concept Catchable = requires(T &obj) {
+  {
+    obj.what()
+  } -> std::convertible_to<std::string>;
+};
+}// namespace anywho::concepts
